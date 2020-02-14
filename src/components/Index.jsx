ebb85d056e6270eb08587a99aec839fb69420b23
walesyu <wales520@gmail.com>
2020-02-14 14:53:06 +0800

    驗證自動發布github page

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -6,19 +6,20 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import Grid from '@material-ui/core/Grid'
 import MyButton from "./MyComponent";
 import {MySpan} from "./MyComponent";
-import {Paper} from "@material-ui/core";
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {color: "red"};
+    let time = new Date();
+    this.state.time = '今天是' + time.getFullYear() + "/" + (time.getMonth() + 1) + "/" + time.getDate();
   }
 
   render = () => {
     return (
         <Grid container spacing={3}>
-          <Grid item md={12} xs={12} style={{'textAlign':'center'}}>
-           <img src={logo} className="App-logo" alt="logo"/>
+          <Grid item md={12} xs={12} style={{'textAlign': 'center'}}>
+            <img src={logo} className="App-logo" alt="logo"/>
           </Grid>
           <Grid item md={6} xs={12} className="center">
             <MyButton variant="contained" color="primary" text="hello" slot={<HomeIcon/>}/>
@@ -27,7 +28,7 @@ class Index extends React.Component {
             <MyButton variant="contained" color="secondary" text="hi" slot={<DeleteIcon/>}/>
           </Grid>
           <Grid item md={12} xs={12} className="center">
-            <MySpan text={'ba la ba ba ba'}/>
+            {this.state.time}<MySpan text={'ba la ba ba ba'}/>
           </Grid>
         </Grid>
     );
