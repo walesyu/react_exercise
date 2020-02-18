e0c597e08aea5b36d2ca912ad0c11c0816241f29
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-18 07:49:14 +0000

    Deploy walesyu/react_exercise to github.com/walesyu/react_exercise.git:gh-pages

--- src/actions/todoList.js
+++
@@ -1,8 +0,0 @@
-//定義Action的資料型態
-export const addTodo = (payload: { id: number, text: string }) => {
-  return {type: 'ADD_ITEM', payload};
-};
-
-export const removeTodo = (id: string) => {
-  return {type: 'DEL_ITEM', id}
-};
